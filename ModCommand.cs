51499138b3a39e2e76759a6fc2a7e10255c26df2
Black_Moss <kytheminecraft@outlook.com>
2026-05-10 16:17:29 +0800

    HOLY SHIFT

---
+++ ModCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using BepInEx.Logging;
+using HarmonyLib;
+using KrokoshaCasualtiesMP;
+using MossLib;
+using MossLib.Base;
+using UnityEngine;
+
+namespace CustomFungamePack;
+
+public class ModCommand : ModCommandBase
+{
+    private static ModCommand _instance;
+    private static readonly ManualLogSource Logger = Plugin.Logger;
+    private static readonly WorldGeneration WorldGeneration = WorldGenerationPatch.WorldGeneration;
+
+    private static ModCommand Instance { get; set; } = new();
+
+    public void Initialize()
+    {
+        if (_instance != null)
+            return;
+        _instance = new ModCommand();
+        Instance = _instance;
+        _instance.Initialize();
+    }
+
+    [HarmonyPatch(typeof (ConsoleScript), "RegisterAllCommands")]
+    public class ConsoleScriptRegisterAllCommandsPatcher
+    {
+        [HarmonyPostfix]
+        public static void RegisterCustomCommands()
+        {
+            ConsoleScript.Commands.Add(new Command(
+                "tiletest", 
+                "testhello.description", _ => 
+                { 
+                    Tools.CheckForWorld();
+                    for (int x = 0; x < 10; x++)
+                    {
+                        for (int y = 0; y < 10; y++)
+                        {
+                            var pos = new Vector2(x,y);
+                            Info(pos.ToString());
+                            var blockPos = WorldGeneration.world.WorldToBlockPos(pos);
+                            WorldGeneration.world.SetBlock(blockPos, 6);
+                        }
+                    }
+                }, 
+                null
+                )
+            );
+        }
+    }
+    
+    private static void Info(string text)
+    {
+        Tools.LogInfo(text, Logger);
+    }
+}
--- WorldGenerationPatch.cs
+++ WorldGenerationPatch.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
-using System.Reflection;
+﻿using System;
 using BepInEx.Logging;
 using HarmonyLib;
+using MossLib;
 using UnityEngine;
+using System.Reflection;
+using Debug = System.Diagnostics.Debug;
 
 namespace CustomFungamePack;
 
@@ -10,153 +12,74 @@ namespace CustomFungamePack;
 public class WorldGenerationPatch
 {
     private static readonly ManualLogSource Logger = Plugin.Logger;
+    internal static WorldGeneration WorldGeneration;
 
-    // 强制开启教程世界
     [HarmonyPatch("Awake")]
     [HarmonyPostfix]
     public static void ForceEnableTutorial(WorldGeneration __instance)
     {
-        __instance.biomeOverride = WorldGeneration.OverrideSceneType.Debug;
+        WorldGeneration = __instance;
+        __instance.biomeOverride = WorldGeneration.OverrideSceneType.Tutorial;
+        Info("已强制修改为教程世界");
     }
-    
-    // // 跳过地形生成
-    // [HarmonyPatch("WorldGenerateTerrain")]
-    // [HarmonyPrefix]
-    // public static bool SkipTerrainGeneration()
-    // {
-    //     return false;
-    // }
-    //
-    // // 跳过结构生成
-    // [HarmonyPatch("WorldGenerateStructures")]
-    // [HarmonyPrefix]
-    // public static bool SkipStructureGeneration()
-    // {
-    //     return false;
-    // }
-    //
-    // // 跳过背景生成
-    // [HarmonyPatch("WorldCreateBackground")]
-    // [HarmonyPrefix]
-    // public static bool SkipWorldCreateBackground()
-    // {
-    //     return false;
-    // }
-    // 跳过实体放置
-    // [HarmonyPatch("WorldPlaceEntities")]
-    // [HarmonyPrefix]
-    // public static bool SkipEntityPlacement()
-    // {
-    //     return false;
-    // }
 
-    // 替换整个 GenerateWorld 协程
-    // [HarmonyPatch("GenerateWorld")]
-    // [HarmonyPrefix]
-    // public static bool ReplaceGenerateWorld(WorldGeneration __instance)
-    // {
-    //     // 如果是教程模式，使用原版生成流程
-    //     if (__instance.biomeOverride == WorldGeneration.OverrideSceneType.Tutorial)
-    //     {
-    //         Logger.LogInfo("[CustomFungamePack] 检测到教程模式，使用原版教程生成流程");
-    //         // 通过反射调用私有的 GenerateWorld 方法
-    //         var generateWorldMethod = typeof(WorldGeneration).GetMethod("GenerateWorld", 
-    //             BindingFlags.NonPublic | BindingFlags.Instance);
-    //         if (generateWorldMethod != null)
-    //         {
-    //             var coroutine = generateWorldMethod.Invoke(__instance, null) as IEnumerator;
-    //             if (coroutine != null)
-    //             {
-    //                 __instance.StartCoroutine(coroutine);
-    //             }
-    //         }
-    //         else
-    //         {
-    //             Logger.LogError("[CustomFungamePack] 未找到 GenerateWorld 方法");
-    //         }
-    //         return false;
-    //     }
-    //     
-    //     // 非教程模式使用自定义生成
-    //     __instance.StartCoroutine(CustomWorldGeneration(__instance));
-    //     return false;
-    // }    
-    // // 跳过层级修饰符
-    // [HarmonyPatch("ApplyLayerModifiers")]
-    // [HarmonyPrefix]
-    // public static bool SkipApplyLayerModifiers()
-    // {
-    //     return false;
-    // }
-    
-    // // 跳过迷你桶
-    // [HarmonyPatch("DistributeMiniBarrels")]
-    // [HarmonyPrefix]
-    // public static bool SkipDistributeMiniBarrels()
-    // {
-    //     return false;
-    // }
-    // private static IEnumerator CustomWorldGeneration(WorldGeneration worldGen)
-    // {
-    //     worldGen.loadingObject.SetActive(true);
-    //     worldGen.generatingWorld = true;
-    //
-    //     yield return InvokeCoroutine(worldGen, "WorldPreprocess");
-    //     yield return InvokeCoroutine(worldGen, "WorldCreateBackground");
-    //     yield return null;
-    //     if (PlayerCamera.main != null && PlayerCamera.main.body != null)
-    //     {
-    //         PlayerCamera.main.body.PlaceBody();
-    //     }
-    //     yield return null;
-    //
-    //     yield return InvokeCoroutine(worldGen, "FinishWorldGeneration");
-    //     yield return null;
-    //     PlaceCustomBlocks(worldGen);
-    // }
-    
-    private static void PlaceCustomBlocks(WorldGeneration worldGen)
+    [HarmonyPatch("FinishWorldGeneration")]
+    [HarmonyPostfix]
+    public static void InitializationWorld(WorldGeneration __instance)
     {
-        Logger.LogInfo("[CustomFungamePack] 开始放置自定义物块");
-        
-        uint width = worldGen.width;
-        uint height = worldGen.height;
-        
-        int centerX = (int)(width / 2);
-        int centerY = (int)(height / 2);
-        
-        for (int x = centerX - 10; x < centerX + 100; x++)
+        for (int x = -76; x < 74; x++)
         {
-            for (int y = centerY - 5; y < centerY + 50; y++)
+            for (int y = -120; y < 58; y++)
             {
-                if (x >= 0 && x < width && y >= 0 && y < height)
+                SetBlock(x, y, 0);
+                Info($"{x}, {y}");
+            }
+        }
+        var chunksField = typeof(WorldGeneration).GetField("chunks", BindingFlags.NonPublic | BindingFlags.Instance);
+        Debug.Assert(chunksField != null, nameof(chunksField) + " != null");
+        if (chunksField.GetValue(__instance) is not Array chunks) return;
+        int upperBound1 = chunks.GetUpperBound(0);
+        int upperBound2 = chunks.GetUpperBound(1);
+            
+        for (int lowerBound1 = chunks.GetLowerBound(0); lowerBound1 <= upperBound1; ++lowerBound1)
+        {
+            for (int lowerBound2 = chunks.GetLowerBound(1); lowerBound2 <= upperBound2; ++lowerBound2)
+            {
+                var chunk = chunks.GetValue(lowerBound1, lowerBound2) as UnityEngine.Tilemaps.Tilemap;
+                if (chunk != null)
                 {
-                    worldGen.SetBlock(new Vector2Int(x, y), 6);
+                    __instance.CreateBackground("steelBackground", chunk);
                 }
             }
         }
-        
-        Logger.LogInfo("[CustomFungamePack] 自定义物块放置完成 - 放置了钢铁方块");
     }
-
-    // private static IEnumerator InvokeCoroutine(WorldGeneration instance, string methodName)
-    // {
-    //     var method = typeof(WorldGeneration).GetMethod(methodName, 
-    //         BindingFlags.NonPublic | BindingFlags.Instance);
-    //     
-    //     if (method != null)
-    //     {
-    //         var result = method.Invoke(instance, null);
-    //         if (result is not IEnumerator coroutine) yield break;
-    //         while (coroutine.MoveNext())
-    //         {
-    //             yield return coroutine.Current;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         Logger.LogError($"[CustomFungamePack] 未找到方法: {methodName}");
-    //         yield return null;
-    //     }
-    // }
+    
+    public static void SetBlock(int x, int y, ushort block)
+    {
+        Vector2 vector2 = new(x, y);
+        SetBlock(vector2, block);
+    }
+    
+    public static void SetBlock(Vector2 vector2, ushort block)
+    {
+        Tools.CheckForWorld();
+        try
+        {
+            WorldGeneration.world.SetBlock(WorldGeneration.world.WorldToBlockPos(vector2), block);
+        }
+        catch (Exception e)
+        { 
+            Error($"在 {vector2} 生成 {block} 失败：{e}");
+        }
+    }
+    
+    private static void Info(string text)
+    {
+        Tools.LogInfo(text, Logger);
+    }
+    
+    private static void Error(string text)
+    {
+        Tools.LogError(text, Logger);
+    }
 }
